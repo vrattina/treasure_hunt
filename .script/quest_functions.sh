7cf411dfd57b22d84b10736da579ad68b238c112
Robin Engler <robin.engler@sib.swiss>
2021-09-26 23:22:09 +0200

    Update shared functions

--- .script/quest_functions.sh
+++ .script/quest_functions.sh
@@ -1,8 +1,16 @@
-KEY_1="lock_1/key_1"
-KEY_2="lock_2/key_2"
-KEY_3="lock_3/key_3"
+KEY_1="treasure_chest/lock_1/key_1"
+KEY_2="treasure_chest/lock_2/key_2"
+KEY_3="treasure_chest/lock_3/key_3"
 CREW="pirate_code.md"
 PIRATES=( "Captain" "Quartermaster" "First mate" )
+FLAG="supplies/flag.ascii"
+RHUM="supplies/rhum.ascii"
+SHIP="supplies/ship.ascii"
+if [[ -z $SHASUM_CMD ]]; then
+    SHASUM_CMD="shasum -a 256"
+else
+    echo "Using custom shasum command: $SHASUM_CMD"
+fi
 
 function random_exclamation {
     exclamations=( "Blimey!" "Sail Ho!" "Shiver me timbers!" "Sink me!" "Aaaarrrrgggghhhh!" "Blimey!" "Sail Ho!" "All Hand Hoy!" "Sink me!" "Scupper that!" )
@@ -21,5 +29,5 @@ function parrot_say {
 
 function get_key_code {
     key_nb=$1
-    cat .support_files/$(sha256sum "lock_${key_nb}/key_${key_nb}" | cut -c1-64) | cut -c$(($key_nb*2-1)),$(($key_nb*2+3))
+    cat .support_files/$(${SHASUM_CMD} "treasure_chest/lock_${key_nb}/key_${key_nb}" | cut -c1-64) | cut -c$(($key_nb*2-1)),$(($key_nb*2+3))
 }
